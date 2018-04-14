3596ee90981ff8b66a193b4adcba8a9edf099547
Joanna Kolny-Olesiak <joanna@Joannas-Air.fritz.box>
2018-04-14 21:27:16 +0200

    Add parent-child relationship

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -12,6 +12,7 @@ struct AcronymsController: RouteCollection {
         acronymRoutes.get("search", use: searchHandler)
         acronymRoutes.get("first", use: getFirstHandler)
         acronymRoutes.get("sorted", use: sortedHandler)
+        acronymRoutes.get(Acronym.parameter, "user", use: getUserHandler)
     }
     
     func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
@@ -30,6 +31,7 @@ struct AcronymsController: RouteCollection {
         return try flatMap(to: Acronym.self, req.parameter(Acronym.self), req.content.decode(Acronym.self), { (acronym, updatedAcronym) in
             acronym.short = updatedAcronym.short
             acronym.long = updatedAcronym.long
+            acronym.userID = updatedAcronym.userID
             return acronym.save(on: req)
         })
     }
@@ -61,5 +63,11 @@ struct AcronymsController: RouteCollection {
     
     func sortedHandler(_ req: Request) throws -> Future<[Acronym]> {
         return try Acronym.query(on: req).sort(\.short, .ascending).all()
+    }
+    
+    func getUserHandler(_ req: Request) throws -> Future<User> {
+        return try req.parameter(Acronym.self).flatMap(to: User.self, { (acronym) in
+            try acronym.user.get(on: req)
+        })
     }
 }
